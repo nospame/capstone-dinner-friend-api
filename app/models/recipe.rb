64bfdd116843bff825a6319b0a809207acc7bb9a
nospame <mapson.evan@gmail.com>
2022-04-21 09:40:00 -0700

    add model associations

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,7 @@
 class Recipe < ApplicationRecord
+  has_many :recipe_tags
+  has_many :tags, through: :recipe_tags
+  has_many :recipe_ingredients
+  has_many :ingredients, through: :recipe_ingredients
+  has_many :steps
 end
