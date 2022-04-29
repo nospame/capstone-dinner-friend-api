0fdde3a869212c85e626e6ad723a70d9f0213a58
nospame <mapson.evan@gmail.com>
2022-04-29 11:19:16 -0700

    add offset param and remove conditional for recipes index

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,18 +1,19 @@
 class RecipesController < ApplicationController
   def index
-    if params[:query]
-      # only returns matching ingredients with data - for all ingredients use .joins
-      @recipes = Recipe.includes(:ingredients, :tags)
-      .where('recipes.name LIKE :query OR ingredients.name LIKE :query', query: "%#{params[:query]}%")
-      .distinct.limit(20).references(:ingredients, :tags)
-    else
-      @recipes = Recipe.includes(:ingredients, :tags).all.limit(20)
-    end
+    query = params[:query] || ''
+    offset = params[:offset] || 0
+    # only returns matching ingredients with data - for all ingredients use .joins
+    @recipes = Recipe.includes(:ingredients, :tags)
+      .where('recipes.name LIKE :query OR ingredients.name LIKE :query', query: "%#{query}%")
+      .limit(20)
+      .references(:ingredients, :tags)
+      .order('recipes.id')
+      .offset(offset)
     render template: "recipes/index"
   end
 
   def show
-    @recipe = Recipe.find_by(id: params[:id])
+    @recipe = Recipe.includes(:ingredients).find_by(id: params[:id])
     render template: "recipes/show"
   end
 end
