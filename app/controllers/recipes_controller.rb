9e45ca44e0d7249be60f99f9b4a0cf8b591d45c0
nospame <mapson.evan@gmail.com>
2022-05-06 13:28:57 -0700

    add tags param and search by tag_ids, code needs DRYing

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,13 +2,27 @@ class RecipesController < ApplicationController
   def index
     query = params[:query] || ''
     offset = params[:offset] || 0
+
     # only returns matching ingredients with data - for all ingredients use .joins
-    @recipes = Recipe.includes(:ingredients, :tags)
+    if params[:tags]
+      tag_ids = params[:tags].split(',')
+      @recipes = Recipe.includes(:ingredients, :tags)
       .where('recipes.name ILIKE :query OR ingredients.name ILIKE :query', query: "%#{query}%")
+      .and(Tag.where(id: tag_ids))
+      .group('recipes.id, ingredients.id, tags.id')
       .limit(20)
       .references(:ingredients, :tags)
       .order('recipes.id')
       .offset(offset)
+    else
+      @recipes = Recipe.includes(:ingredients, :tags)
+      .where('recipes.name ILIKE :query OR ingredients.name ILIKE :query', query: "%#{query}%")
+      .group('recipes.id, ingredients.id, tags.id')
+      .limit(20)
+      .references(:ingredients, :tags)
+      .order('recipes.id')
+      .offset(offset)
+    end
     render template: "recipes/index"
   end
 
